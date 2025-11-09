1730ff80f735929ded6a309c455dc5e8148e9fa6
Ritvik Raj <143361673+ritvikraj115@users.noreply.github.com>
2025-11-09 16:06:55 +0530

    Checking for deployment error

--- src/pages/Kalpavruksha.jsx
+++ src/pages/Kalpavruksha.jsx
@@ -607,8 +607,8 @@ const KalpavrukshaPage = () => {
             </p>
             <div className='mt-6'>
               {/* <!-- Elfsight Google Reviews | Untitled Google Reviews --> */}
-                <script src="https://elfsightcdn.com/platform.js" async></script>
-                <div class="elfsight-app-60cbc103-358f-491e-9cd9-e210af3e21a9" data-elfsight-app-lazy></div>
+                {/* <script src="https://elfsightcdn.com/platform.js" async></script>
+                <div class="elfsight-app-60cbc103-358f-491e-9cd9-e210af3e21a9" data-elfsight-app-lazy></div> */}
             </div>
           </div>
         </div>
